2de03228836cd81893709ec922ad67599f7df98c
ast_lhloba <l.hloba@astoundcommerce.com>
2024-01-19 14:47:46 +0200

    Initian commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'bc3302b8-bd01-423a-a8b8-0d6730e7a7bd';
+export const MODAL_CLOSE_SEC = 2.5;
