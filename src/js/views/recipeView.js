acf225e4dd4588a321e7b8b90a81f119c87ddd92
ast_lhloba <l.hloba@astoundcommerce.com>
2024-01-19 18:31:54 +0200

    Replace Fractional to fracty

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,7 +1,7 @@
 import View from './View.js';
 
 import icons from 'url:../../img/icons.svg';
-import {Fraction} from 'fractional';
+import fracty from 'fracty';
 
 export class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -115,7 +115,7 @@ export class RecipeView extends View {
             <svg class="recipe__icon">
             <use href="${icons}svg#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+            <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity) : ''}</div>
             <div class="recipe__description">
             <span class="recipe__unit">${ing.unit}</span>
             ${ing.description}
